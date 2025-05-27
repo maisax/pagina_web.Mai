97dd8af2a9aaf869f45eca1eebbfde6d460cc097
maisax <maisa.catunda@escola.pr.gov.br>
2025-05-27 14:50:57 -0300

    Add files via upload
    Flashcards finalizados

--- perguntas.js
+++ perguntas.js
@@ -1,22 +1,62 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Anime Awards ',
+    'Anime do ano',
+    'Solo leveling'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Anime Awards',
+    'Melhor filme de animação do ano',
+    'Look back'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Anime Awards',
+    ' Melhor continuação',
+    'Demon Slayer: Kimetsu no Yaiaba Hashira Training Arc'
 )
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em inglês?',
-    'Oi em inglês se diz HI (RAI)'
+    'Anime Awards',
+    'Melhor série estreante',
+    'Solo Leveling'
+)
+criaCartao(
+    'Anime Awards',
+    'Melhor anime original',
+    'Ninja Kamui'
+)
+criaCartao(
+    'Anime Awards',
+    'Melhor animação',
+    'Demon Slayer: Kimetsu no Yaiaba Hashira Training Arc'
+)
+criaCartao(
+    'Anime Awards',
+    'Melhor com design de personagens',
+    'Dandadan'
+)
+criaCartao(
+    'Anime Awards',
+    'Melhor anime de romance',
+    'Blue Box'
+)
+criaCartao(
+    'Anime Awards',
+    'A melhor direção',
+    'Keiichiro Saito (Frieren )'
+)
+criaCartao(
+    'Anime Awards',
+    'Melhor Cenário Artístico',
+    'Frieren e a Jornada para o Além'
+)
+criaCartao(
+    'Anime Awards',
+    'Melhor Anime de Comédia',
+    'MASHLE: MAGIA E MÚSCULOS The Divine Visionary Candidate Exam Arc'
+)
+criaCartao(
+    'Anime Awards',
+    'Melhor Anime Isekai',
+    'Re:ZERO -Starting Life in Another World- 3ª Temporada'
 )
