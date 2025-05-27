919545d80c5ae45bb772b131b8d230b8561d1ded
maisax <maisa.catunda@escola.pr.gov.br>
2025-05-27 01:16:15 -0300

    Add files via upload
    termino da estrutura do alura, porém sem customização

--- perguntas.js
+++ perguntas.js
@@ -15,3 +15,8 @@ criaCartao(
     'O que é uma função?',
     'Uma função é um bloco de código que executa alguma tarefa'
 )
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em inglês?',
+    'Oi em inglês se diz HI (RAI)'
+)
